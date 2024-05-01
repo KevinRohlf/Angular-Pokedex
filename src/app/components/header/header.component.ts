1d09576b41b509f7727fc920721009f7958d13d0
KevinRohlf <kevin@rohlf.io>
2024-05-01 21:44:12 +0200

    Navigate to the next targeted Pokémon using arrow keys and now its navigate to the next search result

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,33 +1,28 @@
 import { Component, input } from '@angular/core';
 import { LoadedPokemonService } from '../../services/loaded-pokemon.service';
-import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
   inputField: string;
 
-  constructor(private pokemonService: LoadedPokemonService) {
-
-  }
+  constructor(private pokemonService: LoadedPokemonService) {}
 
   search(event: any) {
-    if (this.inputField === "") {
+    if (this.inputField === '') {
       this.pokemonService.search = false;
       this.pokemonService.pokemonSearchList = [];
 
       return;
     } else {
       this.pokemonService.search = true;
-      this.pokemonService.pokemonSearchList = this.pokemonService.pokemonFullList.filter((pokemon) => {
-        return pokemon.includes(this.inputField.toLowerCase());
-      });
+      this.pokemonService.pokemonSearchList =
+        this.pokemonService.pokemonFullList.filter((pokemon) => {
+          return pokemon.includes(this.inputField.toLowerCase());
+        });
     }
-
   }
-
-
 }
--- src/app/components/selected-pokemon/selected-pokemon.component.ts
+++ src/app/components/selected-pokemon/selected-pokemon.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, HostListener } from '@angular/core';
 import { Pokemon } from '../../../models/pokemon.class';
 import { LoadedPokemonService } from '../../services/loaded-pokemon.service';
 
@@ -21,20 +21,35 @@ export class SelectedPokemonComponent implements OnInit{
   }
   async ngOnChanges() {
     this.pokemon = new Pokemon(await this.pokemonService.loadPokemon(this.selectedPokemon));
-    this.pokemonIndex = this.pokemonService.pokemonFullList.indexOf(this.pokemon.name);
-    console.log(this.pokemon.abilities)
+    this.pokemonIndex = this.pokemonService.search ? this.pokemonService.pokemonSearchList.indexOf(this.pokemon.name) : this.pokemonService.pokemonFullList.indexOf(this.pokemon.name);
   }
 
   /**
    * Load the next pokemon
    */
   async loadNextPokemon(lastOrNext: string) {
-    if (lastOrNext === "last") {
-      this.pokemonIndex--;
-    } else {
-      this.pokemonIndex++;
-    }
-    this.selectedPokemon = this.pokemonService.pokemonFullList[this.pokemonIndex];
+    if (this.pokemonService.search)
+      this.pokemonIndex = this.pokemonService.pokemonSearchList.indexOf(this.pokemon.name);
+
+    lastOrNext === "last" ? this.pokemonIndex-- : this.pokemonIndex++;
+    this.pokemonService.search ? this.selectedPokemon = this.pokemonService.pokemonSearchList[this.pokemonIndex] : this.selectedPokemon = this.pokemonService.pokemonFullList[this.pokemonIndex];
     this.pokemon = new Pokemon(await this.pokemonService.loadPokemon(this.selectedPokemon));
   }
+
+  @HostListener('document:keydown', ['$event'])
+  handleKeyboardEvent(event: KeyboardEvent) { 
+    if (event.keyCode === 37 && this.pokemonIndex !== 0) {
+      this.loadNextPokemon("last");
+    } else if (event.keyCode === 39 && this.pokemonIndex !== this.pokemonService.pokemonFullList.length - 1) {
+      if (this.pokemonService.search){
+        this.pokemonIndex = this.pokemonService.pokemonSearchList.indexOf(this.pokemon.name);
+        if (this.pokemonIndex < this.pokemonService.pokemonSearchList.length - 1) {
+          this.loadNextPokemon("next");
+        }
+      } else {
+        this.loadNextPokemon("next");
+      }
+    }
+  }
+  
 }
